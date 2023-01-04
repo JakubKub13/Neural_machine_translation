506775e318b70c183e748454f0d201840d7b0c33
JakubKub13 <jakubkubala3@gmail.com>
2023-01-04 08:07:42 +0100

    Adding transformers

--- neural_machine_translation.py
+++ neural_machine_translation.py
@@ -24,3 +24,25 @@ sentence_bleu([tokens], tokens)
 
 sentence_bleu([['hi']], ['hi'])
 
+smoother = SmoothingFunction()
+sentence_bleu(['hi'], 'hi', smoothing_function=smoother.method4)
+
+sentence_bleu(['hi there'.split()], 'hi there'.split())
+
+sentence_bleu(['hi there friend'.split()], 'hi there friend'.split())
+
+sentence_bleu([[1,2,3,4]], [1,2,3,4])
+
+eng2spa_tokens = {}
+for eng, spa_list in eng2spa.items():
+  spa_list_tokens = []
+  for text in spa_list:
+    tokens = tokenizer.tokenize(text.lower())
+    spa_list_tokens.append(tokens)
+  eng2spa_tokens[eng] = spa_list_tokens
+
+#!pip install transformers sentencepiece transformers[sentencepiece]
+
+from transformers import pipeline
+translator = pipeline("translation", model='Helsinki-NLP/opus-mt-en-es', device=0)
+
